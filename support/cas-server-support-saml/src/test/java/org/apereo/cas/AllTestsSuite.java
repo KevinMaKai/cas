c9e2635e1d26867a05478ebbd33b12a7165fa724
Timur Duehr <tduehr@gmail.com>
2018-12-13 19:10:20 -0600

    tests suite fix

--- support/cas-server-support-saml/src/test/java/org/apereo/cas/AllTestsSuite.java
+++ support/cas-server-support-saml/src/test/java/org/apereo/cas/AllTestsSuite.java
@@ -1,5 +1,6 @@
 package org.apereo.cas;
 
+import org.apereo.cas.support.saml.authentication.Saml20ObjectBuilderTests;
 import org.apereo.cas.support.saml.authentication.SamlAuthenticationMetaDataPopulatorTests;
 import org.apereo.cas.support.saml.authentication.SamlAuthenticationRequestTests;
 import org.apereo.cas.support.saml.authentication.principal.SamlServiceFactoryTests;
@@ -24,6 +25,7 @@ import org.junit.platform.suite.api.SelectClasses;
     SamlCompliantUniqueTicketIdGeneratorTests.class,
     WebUtilTests.class,
     SamlServiceFactoryTests.class,
+    Saml20ObjectBuilderTests.class,
     Saml10FailureResponseViewTests.class,
     Saml10SuccessResponseViewTests.class
 })
